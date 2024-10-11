e01749b320902aed4eaca9e2744ae4b127ee3dbc
manxix69 <manxix69@gmail.com>
2024-10-11 10:03:23 +0500

    создал сохрание в БД пользователей при регистрации добавил проверку входа через БД. смену пароля пока не удалось /set_password

--- src/main/java/ru/skypro/homework/controller/AuthController.java
+++ src/main/java/ru/skypro/homework/controller/AuthController.java
@@ -2,13 +2,17 @@ package ru.skypro.homework.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.skypro.homework.dto.Login;
+import ru.skypro.homework.dto.NewPassword;
 import ru.skypro.homework.dto.Register;
 import ru.skypro.homework.service.AuthService;
 
@@ -36,5 +40,36 @@ public class AuthController {
         } else {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
+    }
+
+    @PostMapping("/users/me")
+    public ResponseEntity<?> setNewPassword() {
+        log.info("me: {}, {}"/*, authentication*/);
+        /*if (!authentication.isAuthenticated()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+//        else
+            if (service.setNewPassword(newPassword, authentication)) {
+            return ResponseEntity.status(HttpStatus.OK).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }*/
+        return null;
+    }
+
+        @PostMapping("/users/set_password")
+//    @PostMapping("/profile")
+    public ResponseEntity<?> setNewPassword(@RequestBody NewPassword newPassword, Authentication authentication) {
+        log.info("setNewPassword: {}, {}", newPassword/*, authentication*/);
+        /*if (!authentication.isAuthenticated()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+//        else
+            if (service.setNewPassword(newPassword, authentication)) {
+            return ResponseEntity.status(HttpStatus.OK).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }*/
+        return null;
     }
 }
