33e523230f01b1de7b7d0b1611e806e4d5317eaa
manxix69 <manxix69@gmail.com>
2024-10-20 19:56:14 +0500

    добавил логи в класс BasicAuthCorsFilter

--- src/main/java/ru/skypro/homework/filter/BasicAuthCorsFilter.java
+++ src/main/java/ru/skypro/homework/filter/BasicAuthCorsFilter.java
@@ -1,8 +1,11 @@
 package ru.skypro.homework.filter;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import ru.skypro.homework.controller.UserController;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -13,11 +16,15 @@ import java.io.IOException;
 @Component
 public class BasicAuthCorsFilter extends OncePerRequestFilter {
 
+    private final Logger logger = LoggerFactory.getLogger(BasicAuthCorsFilter.class);
+
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest,
                                     HttpServletResponse httpServletResponse,
                                     FilterChain filterChain)
             throws ServletException, IOException {
+        logger.info("Запущен метод фильтра doFilterInternal {}, {}, {}", httpServletRequest, httpServletResponse, filterChain);
+
         httpServletResponse.addHeader("Access-Control-Allow-Credentials", "true");
         filterChain.doFilter(httpServletRequest, httpServletResponse);
     }
