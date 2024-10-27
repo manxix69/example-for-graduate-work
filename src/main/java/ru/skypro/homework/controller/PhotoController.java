4ac089b9780563e58ec11d9ea92f238c2ca63770
manxix69 <manxix69@gmail.com>
2024-10-27 20:09:16 +0500

    добавил комментарии для фотокотонроллера в swagger

--- src/main/java/ru/skypro/homework/controller/PhotoController.java
+++ src/main/java/ru/skypro/homework/controller/PhotoController.java
@@ -1,11 +1,17 @@
 package ru.skypro.homework.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.skypro.homework.dto.Ad;
 import ru.skypro.homework.service.impl.PhotoServiceImpl;
 import ru.skypro.homework.utils.LogShifter;
 
@@ -22,6 +28,24 @@ public class PhotoController {
     private final Logger logger = LoggerFactory.getLogger(PhotoController.class);
     private final LogShifter shifter = LogShifter.getLogShifter();
 
+    @Operation(
+            tags = "Картинка",
+            summary = "Получение картинки",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "OK",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized",
+                            content = @Content()
+                    )
+            }
+    )
     @GetMapping("/image/{photoId}")
     public ResponseEntity<byte[]> getPhotoFromSource(@PathVariable Integer photoId) throws IOException {
         shifter.log(logger,"Запущен метод контроллера getPhotoFromSource {}", photoId);
