2b610c66f145b2df7558cecc2871794737d48853
manxix69 <manxix69@gmail.com>
2024-11-02 20:57:05 +0500

    добавил отступы и логи изменил в классе PhotoServiceImpl откорректировал метод getPhoto()

--- src/main/java/ru/skypro/homework/service/impl/PhotoServiceImpl.java
+++ src/main/java/ru/skypro/homework/service/impl/PhotoServiceImpl.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Service;
 import ru.skypro.homework.model.PhotoEntity;
 import ru.skypro.homework.repository.PhotoRepository;
 import ru.skypro.homework.service.PhotoService;
+
 import java.io.IOException;
+
 import ru.skypro.homework.exception.PhotoOnDatabaseIsAbsentException;
 import ru.skypro.homework.exception.PhotoOnPcIsAbsentException;
 import ru.skypro.homework.utils.LogShifter;
@@ -29,23 +31,28 @@ public class PhotoServiceImpl implements PhotoService {
     /**
      * Метод возвращает фото с ПК, а если его там нет по каким-то причинам,
      * то перенаправляет запрос фото в базу данных.
+     *
      * @param photoId
      * @return byte[] массив байт
      * @throws IOException
      */
     public byte[] getPhoto(Integer photoId) throws IOException {
-        shifter.shiftLog(logger,"Запущен метод PhotoServiceImpl.getPhoto(): {}" , photoId);
+        shifter.shiftLog(logger, "Запущен метод PhotoServiceImpl.getPhoto(): {}", photoId);
 
-        PhotoEntity photo = photoRepository.findById(photoId).orElseThrow(PhotoOnDatabaseIsAbsentException::new);
-        shifter.log(logger,"Фото найдено - {}", photo.getData() != null);
-        byte[] data = imageService.getPhotoFromDisk(photo);
-        shifter.log(logger,"data :{}", data);
-
-        if (data == null) {  //Если картинка запрошенная с ПК не получена по какой-то причине, достаем ее из БД
-            shifter.shiftBackLog(logger, "выполнен метод PhotoServiceImpl.getPhoto(),{}", data );
-            return photoRepository.findById(photoId).orElseThrow(PhotoOnPcIsAbsentException::new).getData();
+        byte[] data = null;
+        PhotoEntity photo = null;
+        try {
+            photo = photoRepository.findById(photoId).orElseThrow(PhotoOnDatabaseIsAbsentException::new);
+            shifter.log(logger, "Фото найдено - {}", photo.getData() != null);
+            data = imageService.getPhotoFromDisk(photo);
+            if (data == null) {  //Если картинка запрошенная с ПК не получена по какой-то причине, достаем ее из БД
+                data = photoRepository.findById(photoId).orElseThrow(PhotoOnPcIsAbsentException::new).getData();
+            }
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            shifter.shiftBackLog(logger, "выполнен метод PhotoServiceImpl.getPhoto(): {}, {}", photo, data);
         }
-        shifter.shiftBackLog(logger, "выполнен метод PhotoServiceImpl.getPhoto()" );
         return data; //Если предыдущее условие не выполнилось и с картинкой все в порядке, то достаем ее с ПК
     }
 
